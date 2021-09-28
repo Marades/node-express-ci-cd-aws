8066a1c5abd93287542bcc17cdd0298ffdabb4c1
김건회 <jkl9510357@gmail.com>
2021-09-28 16:33:51 +0900

    world router추가

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get("/", function (req, res) {
   res.send("Hello /");
 });
 
+app.get("/world", function (req, res) {
+  res.send("Hello world");
+});
+
 server.listen(3000, function () {
   console.log("Express server listening on port " + server.address().port);
 });

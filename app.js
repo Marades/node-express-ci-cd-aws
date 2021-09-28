841f5c10c8636a7b55c6744aac3e1da49ff9b0cb
김건회 <jkl9510357@gmail.com>
2021-09-28 16:45:53 +0900

    cd router r추가

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ app.get("/world", function (req, res) {
   res.send("Hello world");
 });
 
+app.get("/cd", function (req, res) {
+  res.send("Hello cd");
+});
+
 server.listen(3000, function () {
   console.log("Express server listening on port " + server.address().port);
 });

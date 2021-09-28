34808540fc636e7b4d7a76d95bd12d78a7aff159
김건회 <jkl9510357@gmail.com>
2021-09-28 16:05:08 +0900

    프로젝트 기본 세팅

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
+
+app.get("/", function (req, res) {
+  res.send("Hello /");
+});
+
+server.listen(3000, function () {
+  console.log("Express server listening on port " + server.address().port);
+});
